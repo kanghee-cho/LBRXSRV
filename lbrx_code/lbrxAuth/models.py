b55111d9bea4b37e4ca3eee72377aceebab0e916
Kanghee (Joe) Cho <khcho.me@outlook.com>
2025-04-11 14:57:59 +0900

    update

--- lbrx_code/lbrxAuth/models.py
+++ lbrx_code/lbrxAuth/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser
 
 class LbrxUser(AbstractUser):
     email = models.EmailField(unique=True)
@@ -7,17 +7,6 @@ class LbrxUser(AbstractUser):
     is_active = models.BooleanField(default=False)
     mfa_enabled = models.BooleanField(default=False)
     mfa_secret = models.CharField(max_length=255, blank=True, null=True)
-
-    groups = models.ManyToManyField(
-        Group,
-        related_name="lbrxuser_set",  # Custom related_name to avoid clashes
-        blank=True,
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name="lbrxuser_set",  # Custom related_name to avoid clashes
-        blank=True,
-    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['nickname']
