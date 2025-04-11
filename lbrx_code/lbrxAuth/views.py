5335a0deb5c336c5756097b0a92a848b3fe3f6b4
Kanghee (Joe) Cho <khcho.me@outlook.com>
2025-04-11 14:32:35 +0900

    update

--- lbrx_code/lbrxAuth/views.py
+++ lbrx_code/lbrxAuth/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import SignupSerializer, VerifyEmailSerializer
+from .serializer import SignupSerializer, VerifyEmailSerializer
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from django.utils.http import urlsafe_base64_decode
